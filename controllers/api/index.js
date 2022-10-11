cc9f9ef9f2cab53d9d0ace349788a716df517b34
PeachDoors <taylorpterrill@gmail.com>
2022-10-11 11:45:51 -0600

    write index.js for api routes

---
+++ controllers/api/index.js
@@ -0,0 +1,13 @@
+//define and export API routes
+
+const router = require('express').Router();
+
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes');
+
+router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
