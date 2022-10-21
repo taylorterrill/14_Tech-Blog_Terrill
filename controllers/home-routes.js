038af6a6a6e41f3ec2fe43e1ae5763043d87f7fb
PeachDoors <taylorpterrill@gmail.com>
2022-10-20 18:26:10 -0600

    create post route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
         'content',
         'date_created',
       ],
-      order: [[ 'date_created', 'DESC']],
+      order: [['date_created', 'DESC']],
       include: [
         {
           model: User,
@@ -41,10 +41,40 @@ router.get('/signup', async (req, res) => {
 });
 
 router.get('/dashboard', withAuth, async (req, res) => {
-  const userPosts = await Post.findAll({ where: { user_id: req.session.user_id }});
+  const userPosts = await Post.findAll({ where: { user_id: req.session.user_id } });
   const posts = userPosts.map(post => post.get({ plain: true }))
 
-  res.render('dashboard', {posts})
+  res.render('dashboard', {
+    posts,
+    logged_in: req.session.logged_in
+  })
+});
+
+router.get('/post/:id', withAuth, async (req, res) => {
+  const onePost = await Post.findOne(
+    {
+      where: { id: req.params.id },
+      // order: [['date_created', 'DESC']],
+      // include: [
+      //   {
+      //     model: User,
+      //     attributes: 'username'
+      //   },
+      //   {
+      //     model: Comment,
+      //     attributes: ['id', 'content', 'date_created', 'post_id', 'user_id'],
+      //     include: {
+      //       model: User,
+      //       attributes: ['username'],
+      //     }
+      //   }
+      // ]
+    });
+  const post = onePost.get({ plain: true })
+  res.render('post', {
+    post,
+    logged_in: req.session.logged_in
+  })
 });
 
 module.exports = router;
